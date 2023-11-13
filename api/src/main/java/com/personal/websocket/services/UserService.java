915fc38ebdef701874a047a6a0c6e565a9fb8567
GabeOP <gabrieloliveirap1@hotmail.com>
2023-11-13 13:29:03 -0300

    cadastro de usuario

--- api/src/main/java/com/personal/websocket/services/UserService.java
+++ api/src/main/java/com/personal/websocket/services/UserService.java
@@ -1,11 +1,13 @@
 package com.personal.websocket.services;
 
 import com.personal.websocket.models.entities.UserEntity;
+import com.personal.websocket.models.exceptions.AlreadyRegisteredException;
 import com.personal.websocket.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,17 @@ public class UserService {
 
   public List<UserEntity> getAll() {
     return repository.findAll();
+  }
+
+  public String register(UserEntity entity) {
+      Optional<UserEntity> verifyNome = repository.findByNome(entity.getNome());
+      Optional<UserEntity> verifyEmail = repository.findByEmail(entity.getEmail());
+
+      if(verifyNome.isPresent() || verifyEmail.isPresent()) {
+        throw new AlreadyRegisteredException("[ERRO] Usuário já cadastrado.");
+      }
+
+      repository.save(entity);
+      return "Usuário cadastrado com sucesso";
   }
 }
